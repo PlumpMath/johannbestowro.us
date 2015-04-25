4647658952291cafade304a2205c312040a62f68
Johann Bestowrous <johann.bestowrous@gmail.com>
2015-04-24 20:54:27 -0400

    hide the old

--- src/johann/handler.clj
+++ src/johann/handler.clj
@@ -1,7 +1,7 @@
 (ns johann.handler
-  (:use compojure.core
+  #_(:use compojure.core
         ring.util.response)
-  (:require [compojure.handler :as handler]
+  #_(:require [compojure.handler :as handler]
             [compojure.route :as route]
             [johann.notes :as notes]
             [johann.async :as async]
@@ -12,52 +12,50 @@
             [ring.middleware.reload :as reload]
             [ring.middleware.edn :refer [wrap-edn-params]]
             [clojure.java.io :as io])
-  (:import [javax.script
+  #_(:import [javax.script
             Invocable
             ScriptEngineManager]))
 
 
 
+(comment
+  (defn uuid [] (str (java.util.UUID/randomUUID)))
 
-(defn uuid [] (str (java.util.UUID/randomUUID)))
+  (defn title->idx [title]
+    (let [post (first (filter #(= (% :title) title) (notes/note-cache-prod)))]
+      (:idx post "not found")))
 
-(defn title->idx [title]
-  (let [post (first (filter #(= (% :title) title) (notes/note-cache-prod)))]
-    (:idx post "not found")))
+  (defroutes app-routes
+    (GET "/" [] (views/dream-page  {:route {:view nil
+                                            :context []
 
-(defroutes app-routes
-  (GET "/" [] (views/dream-page  {:route {:view nil
-                                                :context []
+                                            } :routes {:blog notes/topic-sort}}))
+    (GET "/on/:topic/:title" [topic title] (views/dream-page  {:route {:view "blog"
+                                                                       :context [topic (title->idx title) ]
 
-                                                } :routes {:blog notes/topic-sort}}))
-  (GET "/on/:topic/:title" [topic title] (views/dream-page  {:route {:view "blog"
-                                                                     :context [topic (title->idx title) ]
+                                                                       } :routes {:blog notes/topic-sort}}))
+    (GET "/ws" [] async/async-handler)
+    (route/resources "/" {:root "public"})
+    (route/not-found "Not Found"))
 
-                                                                     } :routes {:blog notes/topic-sort}}))
-  (GET "/ws" [] async/async-handler)
-  (route/resources "/" {:root "public"})
-  (route/not-found "Not Found"))
+  (def app
+    (->
+     app-routes
+     ;;assign-uuid
+     handler/site
+     reload/wrap-reload))
 
-(def app
-  (->
-   app-routes
-   ;;assign-uuid
-   handler/site
-   reload/wrap-reload))
-
-(defn -main
-  ([port]
-    (let [Port (Integer/parseInt port)]
-      (println "server running w/ port" port)
-      (print "mode is " (System/getenv "MODE"))
-      (http-kit/run-server app {:port Port})))
-  ([] ()
-   (let [strPort (System/getenv "PORT")
-         Port (Integer/parseInt strPort)]
-     (println "we're running on:" sort)
-     (http-kit/run-server app {:port Port})) ))
-
-
+  (defn -main
+    ([port]
+     (let [Port (Integer/parseInt port)]
+       (println "server running w/ port" port)
+       (print "mode is " (System/getenv "MODE"))
+       (http-kit/run-server app {:port Port})))
+    ([] ()
+     (let [strPort (System/getenv "PORT")
+           Port (Integer/parseInt strPort)]
+       (println "we're running on:" sort)
+       (http-kit/run-server app {:port Port})) ))
 
 
 
@@ -72,3 +70,6 @@
 
 
 
+
+
+  )
--- src/johann/website.clj
+++ src/johann/website.clj
@@ -1,94 +1,81 @@
-(ns johann.website
-  (:require
-   [clojure.tools.logging :as log]
-   [com.stuartsierra.component :as component]
-   [plumbing.core :refer :all :exclude [update]]
-   [schema.core :as s]
-   [clojure.pprint :refer (pprint)]
-   [modular.ring :refer (WebRequestHandler WebRequestMiddleware)]
-   [modular.bidi :refer (WebService as-request-handler)]
-   [ring.util.response :as resp]
-   [ring.util.request :as req]
-   [ring.middleware.defaults :refer (wrap-defaults site-defaults)]
-   [bidi.bidi :refer (path-for compile-route)]
-   [bidi.ring :refer (redirect files)]
-   [johann.notes :as notes]
-   [johann.views.complector :as views]))
+(ns johann.website)
 
-(def htmlify (fn->
-              (resp/content-type "text/html")))
+(comment
+  (def htmlify (fn->
+                (resp/content-type "text/html")))
 
-(defn title->idx [title]
-  (let [post (first (filter #(= (% :title) title)
-                            (notes/note-cache-prod)))]
-    (:idx post "not found")))
+  (defn title->idx [title]
+    (let [post (first (filter #(= (% :title) title)
+                              (notes/note-cache-prod)))]
+      (:idx post "not found")))
 
-(defrecord StaticFileService [uri-context dir]
-  WebService
-  (request-handlers [_] {})
-  (routes [_]
-    [uri-context (files {:dir dir})])
-  (uri-context [_] "/"))
+  (defrecord StaticFileService [uri-context dir]
+    WebService
+    (request-handlers [_] {})
+    (routes [_]
+      [uri-context (files {:dir dir})])
+    (uri-context [_] "/"))
 
-(def new-static-file-service-schema
-  {:uri-context s/Str
-   :dir s/Str})
+  (def new-static-file-service-schema
+    {:uri-context s/Str
+     :dir s/Str})
 
-(defn new-static-file-service [& {:as opts}]
-  (->> opts
-       (merge {})
-       (s/validate new-static-file-service-schema)
-       (map->StaticFileService)))
+  (defn new-static-file-service [& {:as opts}]
+    (->> opts
+         (merge {})
+         (s/validate new-static-file-service-schema)
+         (map->StaticFileService)))
 
-(defrecord Middleware []
-  WebRequestMiddleware
-  (request-middleware [_]
-    (fn [handler]
-      (let []
-        (-> handler
-            (wrap-defaults
-             (-> site-defaults
-                 (dissoc :static))))))))
+  (defrecord Middleware []
+    WebRequestMiddleware
+    (request-middleware [_]
+      (fn [handler]
+        (let []
+          (-> handler
+              (wrap-defaults
+               (-> site-defaults
+                   (dissoc :static))))))))
 
-(defn new-middleware
-  [& {:as opts}]
-  (->> (map->Middleware {})
-       (<- (component/using []))))
+  (defn new-middleware
+    [& {:as opts}]
+    (->> (map->Middleware {})
+         (<- (component/using []))))
 
-(defrecord Website [html-template ip port transition-duration]
-  WebService
-  (request-handlers [this]
-    {::home (fn [req]
-              (let [state {:route {:view nil
-                                   :context []}
-                           :routes {:blog notes/topic-sort}}
-                    dream (views/dream-page state)]
-                (htmlify (resp/response dream))))
-     ::post (fn [req]
-              (let [title (get-in req [:route-params :title])
-                    topic (get-in req [:route-params :topic])
-                    state  {:route {:view "blog"
-                                    :context [topic (title->idx title)]}
-                            :routes {:blog notes/topic-sort}}
-                    dream (views/dream-page state)]
-                (htmlify (resp/response dream))))})
+  (defrecord Website [html-template ip port transition-duration]
+    WebService
+    (request-handlers [this]
+      {::home (fn [req]
+                (let [state {:route {:view nil
+                                     :context []}
+                             :routes {:blog notes/topic-sort}}
+                      dream (views/dream-page state)]
+                  (htmlify (resp/response dream))))
+       ::post (fn [req]
+                (let [title (get-in req [:route-params :title])
+                      topic (get-in req [:route-params :topic])
+                      state  {:route {:view "blog"
+                                      :context [topic (title->idx title)]}
+                              :routes {:blog notes/topic-sort}}
+                      dream (views/dream-page state)]
+                  (htmlify (resp/response dream))))})
 
-  (routes [_] (compile-route ["/" {"" ::home
-                                   ["on/" :topic :title] ::post}]))
+    (routes [_] (compile-route ["/" {"" ::home
+                                     ["on/" :topic :title] ::post}]))
 
-  (uri-context [_] "")
+    (uri-context [_] "")
 
-  WebRequestHandler
-  (request-handler [this] (as-request-handler this)))
+    WebRequestHandler
+    (request-handler [this] (as-request-handler this)))
 
-(def new-website-schema
-  {:ip s/Str
-   :port s/Int
-   :transition-duration s/Int})
+  (def new-website-schema
+    {:ip s/Str
+     :port s/Int
+     :transition-duration s/Int})
 
-(defn new-website
-  [& {:as opts}]
-  (->> opts
-       (merge {})
-       (s/validate new-website-schema)
-       map->Website))
+  (defn new-website
+    [& {:as opts}]
+    (->> opts
+         (merge {})
+         (s/validate new-website-schema)
+         map->Website))
+  )
