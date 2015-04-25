5930b0bdf75de914470a216722388cbecf9f376b
Johann Bestowrous <johann.bestowrous@gmail.com>
2015-04-24 23:28:55 -0400

    get evernote credentials in scope

--- src/johann/components/evernote.clj
+++ src/johann/components/evernote.clj
@@ -1,12 +1,24 @@
 (ns johann.components.evernote
+  (:import (com.evernote.edam.notestore NoteStore NoteFilter NotesMetadataResultSpec)
+           (com.evernote.edam.type Note Notebook NoteSortOrder))
   (:require [bidi.bidi :refer (RouteProvider)]
+            [clj-time.coerce :as coerce]
+            [clj-time.format :as format]
+            [clojure.core.memoize :as memo]
+            [clojure.edn :as edn]
+            [clojurenote.enml :as enml]
+            [clojurenote.notes :as notes]
+            [clojurenote.users :as users]
             [com.stuartsierra.component :refer (Lifecycle)]
+            [hiccup-bridge.core :as hicv]
             [ib5k.component.ctr :as ctr]
             [plumbing.core :refer :all]
+            [ring.util.response :refer (response content-type)]
             [schema.core :as s]
             [taoensso.timbre :as log]))
 
-(s/defrecord TestRoute [context :- s/Str]
+(s/defrecord EvernoteCache [context :- s/Str notebook-uid :- s/Str access-token :- s/Str
+                            notestore-url :- s/Str]
   Lifecycle
   (start [this]
          this)
@@ -14,12 +26,18 @@
         this)
   RouteProvider
   (routes [this]
-    (vector context {:get (fn [req]
-                            (println req)
-                            req)})))
+          (vector context {:get (fn [req]
+                                  (->> (notes/basic-notes-for-notebook
+                                        {:notestore-url notestore-url
+                                         :access-token access-token} notebook-uid)
+                                       (map (fn [note]
+                                              (.. note getGuid)))
+                                       println)
 
-(def new-test-route
-  (-> map->TestRoute
-      (ctr/wrap-class-validation TestRoute)
+                                  (response "america"))})))
+
+(def new-evernote-cache
+  (-> map->EvernoteCache
+      (ctr/wrap-class-validation EvernoteCache)
       (ctr/wrap-defaults {:context "/"})
       (ctr/wrap-kargs)))
--- src/johann/systems/server.clj
+++ src/johann/systems/server.clj
@@ -2,7 +2,7 @@
   (:require [johann.utils.config :refer [config]]
             [johann.utils.maker :refer [make]]
             [johann.utils.system :refer [new-system]]
-            [johann.components.evernote :refer ( new-test-route)]
+            [johann.components.evernote :refer ( new-evernote-cache)]
             [bidi.bidi :refer (RouteProvider)]
             [modular.bidi]
             [modular.http-kit]
@@ -12,7 +12,10 @@
             [tangrammer.component.co-dependency :as co-dependency]))
 
 (defn components [config]
-  {:test-route {:cmp (make new-test-route config
+  {:evernote-cache {:cmp (make new-evernote-cache config
+                           {:notestore-url [:evernote :notestore-url]} ""
+                           {:access-token [:evernote :access-token]} ""
+                           {:notebook-uid [:evernote :notebook-uid]} ""
                            :context "/")}
    :public-resources
    {:cmp (make modular.bidi/new-web-resources config
@@ -20,7 +23,7 @@
                :resource-prefix "public")}
    :webrouter
    {:cmp (modular.bidi/new-router)
-    :using [:test-route (s/protocol RouteProvider)
+    :using [:evernote-cache (s/protocol RouteProvider)
             :public-resources (s/protocol RouteProvider)]}
    :webhead
    {:cmp (modular.ring/new-web-request-handler-head)
